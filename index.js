6dd5024b271618b667d43d9253dca8dfcf495ec3
sidey7 <sidneyhalleynogueiraferreiraha@gmail.com>
2025-03-19 01:09:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,298 @@
+const botaoMostrar = document.getElementById("bota1");
+const explicaDiv = document.getElementById("expkica");
+const botafecha = document.getElementById("fechar");
+const botavoltar = document.getElementById("none");
+
+botaoMostrar.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv).display === "none") { 
+    explicaDiv.style.display = "block"; 
+  } else { 
+    explicaDiv.style.display = "none"; 
+  }
+});
+
+botavoltar.addEventListener("click", () => {
+  explicaDiv.style.display = "none";
+});
+
+botafecha.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+
+// Segunda seção
+const botaoMostrar1 = document.getElementById("bota3");
+const explicaDiv1 = document.getElementById("expkica1");
+const botafecha1 = document.getElementById("fechar1");
+const botavoltar1 = document.getElementById("none1");
+
+botaoMostrar1.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv1).display === "none") { 
+    explicaDiv1.style.display = "block"; 
+  } else { 
+    explicaDiv1.style.display = "none"; 
+  }
+});
+
+botavoltar1.addEventListener("click", () => {
+  explicaDiv1.style.display = "none";
+});
+
+botafecha1.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+
+// Terceira seção corrigida
+const botaoMostrar2 = document.getElementById("bota4");
+const explicaDiv2 = document.getElementById("expkica2");
+const botafecha2 = document.getElementById("fechar2");
+const botavoltar2 = document.getElementById("none2");
+
+botaoMostrar2.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv2).display === "none") { 
+    explicaDiv2.style.display = "block"; 
+  } else { 
+    explicaDiv2.style.display = "none"; 
+  }
+});
+
+botavoltar2.addEventListener("click", () => {
+  explicaDiv2.style.display = "none";
+});
+
+botafecha2.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+
+// Quarta seção corrigida
+const botaoMostrar3 = document.getElementById("bota5");
+const explicaDiv3 = document.getElementById("expkica3");
+const botafecha3 = document.getElementById("fechar3");
+const botavoltar3 = document.getElementById("none3");
+
+botaoMostrar3.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv3).display === "none") { 
+    explicaDiv3.style.display = "block"; 
+  } else { 
+    explicaDiv3.style.display = "none"; 
+  }
+});
+
+botavoltar3.addEventListener("click", () => {
+  explicaDiv3.style.display = "none";
+});
+
+botafecha3.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+
+// Quarta seção corrigida
+const botaoMostrar4 = document.getElementById("bota6");
+const explicaDiv4 = document.getElementById("expkica4");
+const botafecha4 = document.getElementById("fechar4");
+const botavoltar4 = document.getElementById("none4");
+
+botaoMostrar4.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv4).display === "none") { 
+    explicaDiv4.style.display = "block"; 
+  } else { 
+    explicaDiv4.style.display = "none"; 
+  }
+});
+
+botavoltar4.addEventListener("click", () => {
+  explicaDiv4.style.display = "none";
+});
+
+botafecha4.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+
+
+// Quinta seção corrigida
+const botaoMostrar7 = document.getElementById("bota7");
+const explicaDiv7 = document.getElementById("expkica7");
+const botafecha7 = document.getElementById("fechar7");
+const botavoltar7 = document.getElementById("none7");
+
+botaoMostrar7.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv4).display === "none") { 
+    explicaDiv7.style.display = "block"; 
+  } else { 
+    explicaDiv7.style.display = "none"; 
+  }
+});
+
+botavoltar7.addEventListener("click", () => {
+  explicaDiv7.style.display = "none";
+});
+
+botafecha7.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+
+// sexta seção corrigida
+const botaoMostrar8 = document.getElementById("bota8");
+const explicaDiv8 = document.getElementById("expkica8");
+const botafecha8 = document.getElementById("fechar8");
+const botavoltar8 = document.getElementById("none8");
+
+botaoMostrar8.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv8).display === "none") { 
+    explicaDiv8.style.display = "block"; 
+  } else { 
+    explicaDiv8.style.display = "none"; 
+  }
+});
+
+botavoltar8.addEventListener("click", () => {
+  explicaDiv8.style.display = "none";
+});
+
+botafecha8.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+
+const botaoMostrar9 = document.getElementById("bota9");
+const explicaDiv9 = document.getElementById("expkica9");
+const botafecha9 = document.getElementById("fechar9");
+const botavoltar9 = document.getElementById("none9");
+
+botaoMostrar9.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv9).display === "none") { 
+    explicaDiv9.style.display = "block"; 
+  } else { 
+    explicaDiv9.style.display = "none"; 
+  }
+});
+
+botavoltar9.addEventListener("click", () => {
+  explicaDiv9.style.display = "none";
+});
+
+botafecha9.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+
+
+const botaoMostrar10 = document.getElementById("bota10");
+const explicaDiv10 = document.getElementById("expkica10");
+const botafecha10 = document.getElementById("fechar10");
+const botavoltar10 = document.getElementById("none10");
+
+botaoMostrar10.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv10).display === "none") { 
+    explicaDiv10.style.display = "block"; 
+  } else { 
+    explicaDiv10.style.display = "none"; 
+  }
+});
+
+botavoltar10.addEventListener("click", () => {
+  explicaDiv10.style.display = "none";
+});
+
+botafecha10.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+
+
+
+const botaoMostrar11 = document.getElementById("bota11");
+const explicaDiv11 = document.getElementById("expkica11");
+const botafecha11 = document.getElementById("fechar11");
+const botavoltar11 = document.getElementById("none11");
+
+botaoMostrar11.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv11).display === "none") { 
+    explicaDiv11.style.display = "block"; 
+  } else { 
+    explicaDiv11.style.display = "none"; 
+  }
+});
+
+botavoltar11.addEventListener("click", () => {
+  explicaDiv11.style.display = "none";
+});
+
+botafecha11.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+
+
+const botaoMostrar12 = document.getElementById("bota12");
+const explicaDiv12 = document.getElementById("expkica12");
+const botafecha12 = document.getElementById("fechar12");
+const botavoltar12 = document.getElementById("none12");
+
+botaoMostrar12.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv12).display === "none") { 
+    explicaDiv12.style.display = "block"; 
+  } else { 
+    explicaDiv12.style.display = "none"; 
+  }
+});
+
+botavoltar12.addEventListener("click", () => {
+  explicaDiv12.style.display = "none";
+});
+
+botafecha12.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+
+
+const botaoMostrar13 = document.getElementById("bota13");
+const explicaDiv13 = document.getElementById("expkica13");
+const botafecha13 = document.getElementById("fechar13");
+const botavoltar13 = document.getElementById("none13");
+
+botaoMostrar13.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv13).display === "none") { 
+    explicaDiv13.style.display = "block"; 
+  } else { 
+    explicaDiv13.style.display = "none"; 
+  }
+});
+
+botavoltar13.addEventListener("click", () => {
+  explicaDiv13.style.display = "none";
+});
+
+botafecha13.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+const botaoMostrar14 = document.getElementById("bota14");
+const explicaDiv14 = document.getElementById("expkica14");
+const botafecha14 = document.getElementById("fechar14");
+const botavoltar14 = document.getElementById("none14");
+
+botaoMostrar14.addEventListener("click", () => {
+  if (window.getComputedStyle(explicaDiv14).display === "none") { 
+    explicaDiv14.style.display = "block"; 
+  } else { 
+    explicaDiv14.style.display = "none"; 
+  }
+});
+
+botavoltar14.addEventListener("click", () => {
+  explicaDiv14.style.display = "none";
+});
+
+botafecha14.addEventListener("click", () => {
+  window.location.href = "https://wa.me/5598996099309?text=ola,+tudo+bem+?+queria+entrar+pro+hot";
+});
+
+
+
